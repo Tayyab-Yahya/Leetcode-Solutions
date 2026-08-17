182fe9393190553503d27a35df7a43a6ec4b3f2b
Tayyab Yahya <htayyab009@gmail.com>
2026-08-18 01:06:00 +0500

    feat: add solutions for First and Last Position of Element in Sorted Array and Search Insert Position problems

---
+++ 34-First and Last Position of Element in Sorted Array.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int findFirst(vector<int>& nums, int target) {
+        int s = 0, e = nums.size()-1, ans = -1;
+        while(s<=e){
+            int mid = s+(e-s)/2;
+            if(nums[mid]==target){
+                ans = mid;
+                e = mid-1;
+            } else if (nums[mid]<target)
+                s = mid+1;
+            else
+                e = mid-1;
+        }
+        return ans;
+    }
+
+    int findLast(vector<int>& nums, int target) {
+        int s = 0, e = nums.size()-1, ans = -1;
+        while(s<=e){
+            int mid = s+(e-s)/2;
+            if(nums[mid]==target){
+                ans = mid;
+                s = mid+1;
+            } else if (nums[mid]<target)
+                s = mid+1;
+            else
+                e = mid-1;
+        }
+        return ans;
+    }
+
+    vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int> arr(2);
+        arr[0] = findFirst(nums, target);
+        arr[1] = findLast(nums, target);
+        return arr;
+    }
+};
